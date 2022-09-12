c9d94992ec5a60384b8c3140310803df069086fc
Arthur G. Bertoti <artgbertoti@gmail.com>
2022-09-12 20:16:02 -0300

    módulo 3 concluído

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
+import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/inicio';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-export default function AppRouter(){
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Inicio />} />
-				<Route path="/cardapio" element={<Cardapio />} />
-			</Routes>
-		</Router>
-	);
+export default function AppRouter() {
+    return (
+        <main>
+            <Router>
+                <Menu />
+                <Routes>
+                    <Route path='/' element={<PaginaPadrao />} >
+                        <Route index element={<Inicio/>} />
+                        <Route path='cardapio' element={<Cardapio />} />
+                    </Route>
+                </Routes>
+            </Router>
+        </main>
+    );
 }
